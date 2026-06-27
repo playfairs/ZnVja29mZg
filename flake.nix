28f00a9bd4d7721bb282b5820160b80abd3fb8a2
Invra <identificationsucks@gmail.com>
2026-06-28 02:00:57 +1000

    chore: switch build system to meson

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "ZnVja29mZg SDL3 procedural visuals";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -13,19 +13,16 @@
           pname = "znvja29mzg";
           version = "0.1";
           src = ./.;
-          nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config ];
-          buildInputs = [ pkgs.clang pkgs.sdl3 ];
-          buildPhase = ''
-            cmake -S $SRC -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo
-            cmake --build build -- -j${pkgs.stdenv.hostPlatform.buildParallel}
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            cp build/ZnVja29mZg $out/bin/
-          '';
+          buildInputs = with pkgs; [ meson ninja  python3 ];
+          nativeBuildInputs = with pkgs; [ sdl3  pkg-config ];
         };
         devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.cmake pkgs.clang pkgs.pkg-config pkgs.sdl3 ];
+          buildInputs = with pkgs; [
+            meson
+            ninja
+            pkg-config
+            sdl3
+          ];
           shellHook = ''
             echo "fuckoff"
           '';
